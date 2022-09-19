5761b84809fbd1bb1b872ec5c69f59717d5d954c
Shahbaz-Ali <shahbazalidev@gmail.com>
2022-09-19 19:38:47 +0500

    added some styles to regitration page

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -13,23 +13,23 @@ export default function Register(){
                         Registry.io is best place to find people respective to their profession and Location. </p>
             </div>
 
-            <div className="sm:w-1/2 flex flex-col  sm:p-3 px-3 pb-2 mt-1 sm:pt-1 justify-between  text-md">
+            <div className="sm:w-1/2 flex flex-col  sm:p-3 px-3 pb-2 mt-2 sm:pt-1 justify-between  text-md">
                 <h2 className="sm:text-2xl text-xl sm:hidden font-bold font-serif text-center bg-gradient-to-tr rounded-lg py-1 text-gray-200">
                     Registry.io</h2>
                 <h2 className="text-xl sm:text-2xl fotn-serif font-extrabold text-red-900">Register Here</h2>
-                <p className="text-md mt-1 font-bold text-white-900 outline-2 outline-white">Create your  free account.</p>
-               <input type="email" placeholder="Email"
-                className=" border-2 mt-1 border-gray-400 text-md p-1 outline-none 
+                <p className="text-md mt-2 font-bold text-white-900 outline-2 outline-white">Create your  free account.</p>
+                <input type="email" placeholder="Email"
+                className=" border-2 mt-2 border-gray-400 text-md p-1 outline-none 
                 focus:border-black-900 rounded-md focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000"></input>
-                    <input type="name" placeholder="First Name" className="focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000  hover:border-black/70  border-2 border-gray-400 w-3/4 text-md p-1 outline-none 
-                focus:border-black-900 mt-1 rounded-md" ></input>
-                    <input type="name" placeholder="Last Name" className="border-2 border-gray-400 w-3/4 text-md p-1 outline-none 
-                focus:border-black-900 mt-1 rounded-md focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000" ></input>
+                <input type="name" placeholder="First Name" className="focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000  hover:border-black/70  border-2 border-gray-400 w-3/4 text-md p-1 outline-none 
+                focus:border-black-900 mt-2 rounded-md" ></input>
+                <input type="name" placeholder="Last Name" className="border-2 border-gray-400 w-3/4 text-md p-1 outline-none 
+                focus:border-black-900 mt-2 rounded-md focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000" ></input>
                  <input placeholder="Date Of Birth"  type="date" className="border-2 border-gray-400 w-3/4 text-md p-1 outline-none 
-                focus:border-black-900 rounded-md mt-1 accent-pink-900 focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000" />                
+                focus:border-black-900 rounded-md mt-2 accent-pink-900 focus:shadow-lg focus:shadow-black transition ease-in-out duration-1000" />                
                 <div className="flex">
-                <input type="checkbox" className="accent-black mt-1 hover:accent-pink-600 hover:outline-black hover:outline-4 inline mr-1"/>
-                <p className="text-md mt-1 font-mono">I accept the <span className="text-red-900 font-bold hover:text-white">term of use</span> and <span className="text-red-900 font-bold hover:text-white">privacy policy.</span></p>
+                <input type="checkbox" className="accent-black mt-2 hover:accent-pink-600 hover:outline-black hover:outline-4 inline mr-1"/>
+                <p className="text-md mt-2 font-mono">I accept the <span className="text-red-900 font-bold hover:text-white">term of use</span> and <span className="text-red-900 font-bold hover:text-white">privacy policy.</span></p>
                 </div>
                 
                 <button className="bg-red-900 font-bold text-white rounded-xl py-1 hover:shadow-lg hover:shadow-black transition ease-in-out duration-1000">Register me</button>
